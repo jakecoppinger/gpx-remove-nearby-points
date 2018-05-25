cae6e980cb6b9fca779dbb13a5f81ac67a3575fd
Jake Coppinger <jake@jakecoppinger.com>
2018-05-25 20:11:38 +1000

    Finished

--- gpx-remove-nearby-points.py
+++ gpx-remove-nearby-points.py
@@ -10,6 +10,14 @@ from sufficient_distance_away import sufficient_distance_away
 def pretty(o):
     print(json.dumps(o, indent=2))
 
+def gpx_point_to_point(gpx_point):
+    return (float(gpx_point['@lat']), float(gpx_point['@lon']))
+
+def transform_track(track):
+    new_points = [point for point in track['trkseg']['trkpt'] if 
+        sufficient_distance_away(gpx_point_to_point(point), position, distance)]
+    return {'trkseg':{'trkpt':new_points }}
+
 args = sys.argv[1:]
 if len(args) != 2:
     raise AssertionError("Please give 2 args where lat & long separated by comma: latitude,longitude distance")
@@ -23,42 +31,22 @@ distance = float(args[1])
 
 # Import
 xml_data = sys.stdin.read()
-data_object = xmltodict.parse(xml_data) #,process_namespaces=True
+data_object = xmltodict.parse(xml_data)
 
-original_tracks = data_object['gpx']['trk']
-num_tracks =len(original_tracks)
-print('num tracks:', num_tracks)
-
-def gpx_point_to_point(gpx_point):
-    # print(gpx_point)
-    return (float(gpx_point['@lat']), float(gpx_point['@lon']))
-
-def transform_track(track):
-    new_points = [point for point in track['trkseg']['trkpt'] if 
-        sufficient_distance_away(gpx_point_to_point(point), position, distance)]
-
-
-    return {'trkseg':{'trkpt':new_points } }
-
-# We can't use list comprehensions if only one object in array
-if(num_tracks == 1):
-    old_track = data_object['gpx']['trk']
-    data_object['gpx']['trk'] = transform_track(old_track)
+# Filter points
+num_tracks = len(data_object['gpx']['trk'])
+if num_tracks == 0:
+    raise Exception("There doesn't appear to be a track in the data.")
+elif num_tracks == 1:
+    data_object['gpx']['trk'] = transform_track(data_object['gpx']['trk'])
 else:
-    new_tracks = [transform_track(track) for track in original_tracks]
+    new_tracks = [transform_track(track) for track in data_object['gpx']['trk']]
     data_object['gpx']['trk'] = new_tracks
 
-pretty(data_object['gpx']['trk'])
-
-# Get all the points
-# trkpts = data_object['gpx']['trk']['trkseg']['trkpt']
-
-# # Filter points
-# far_trkpts = [point for point in trkpts if 
-#     sufficient_distance_away(gpx_point_to_point(point), position, distance)]
-
-# # Export back to xml
-# track['gpx']['trk']['trkseg']['trkpt'] = far_trkpts
+# Export back to xml
 xml = xmltodict.unparse(data_object, pretty=True)
 
-# print(xml)
+print(xml)
+
+# Pretty print JSON
+# pretty(data_object['gpx']['trk'])
